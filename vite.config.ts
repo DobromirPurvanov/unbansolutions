425b09884a036dcd94d80e45b40916b85d1a2be3
DobromirPurvanov <dobotox@gmail.com>
2026-07-04 18:38:18 +0300

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,22 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { inspectAttr } from 'kimi-plugin-inspect-react'
 
+// Plugin to make CSS async (non-render-blocking)
+const asyncCssPlugin = () => ({
+  name: 'async-css',
+  transformIndexHtml(html: string) {
+    // Replace render-blocking stylesheet links with async preload
+    return html.replace(
+      /<link rel="stylesheet" crossorigin href="([^"]+)"\s*\/?>/g,
+      '<link rel="preload" href="$1" as="style" onload="this.onload=null;this.rel=\'stylesheet\'" />\n    <noscript><link rel="stylesheet" href="$1" /></noscript>'
+    );
+  },
+});
+
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [inspectAttr(), react()],
+  plugins: [inspectAttr(), react(), asyncCssPlugin()],
   server: {
     port: 3000,
   },
